35c36f0cbe98464a84419bc3bc7e6769118d825a
Shailesh <shaileshsiingh@gmail.com>
2023-03-25 13:54:34 +0530

    third commit

--- dom.js
+++ dom.js
@@ -3,11 +3,19 @@ header = document.getElementById('main-header')
 headerTitle.style.border =  " solid 3px #000";
 
 var items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor = 'green';
+// items[2].style.backgroundColor = 'green';
 
-for(let i = 0; i<=items.length; i++){
+for(let i = 0; i<items.length; i++){
     items[i].style.fontWeight = 'bold';
+    // items[i].style.color = 'green';
 }
+
+var li = document.getElementsByTagName('li');
+for (let i = 0; i < li.length; i++) {
+  li[i].style.backgroundColor = '#f4f4f4';
+}
+
+
 
 
 
