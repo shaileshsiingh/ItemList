8864b98bedc6f56b976a5965de54317a4ffd4fbc
Shailesh <shaileshsiingh@gmail.com>
2023-03-25 13:35:12 +0530

    second commit

--- dom.js
+++ dom.js
@@ -3,7 +3,11 @@ header = document.getElementById('main-header')
 headerTitle.style.border =  " solid 3px #000";
 
 var items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = 'green';
+
 for(let i = 0; i<=items.length; i++){
-    items[i].style.fontWeight = "bold";
-    items[i].style.color = 'green'
+    items[i].style.fontWeight = 'bold';
 }
+
+
+
