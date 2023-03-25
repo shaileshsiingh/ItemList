0eeea5c9be2fe3efe86efc26dc2be926dea88bfd
Shailesh <shaileshsiingh@gmail.com>
2023-03-26 00:07:55 +0530

    fifth commit

--- dom.js
+++ dom.js
@@ -28,15 +28,80 @@
 // var item = document.querySelectorAll('.list-group-item');
 // item[1].style.color = 'green';
 
-var odd = document.querySelectorAll('li:nth-child(odd')
-var even = document.querySelectorAll('li:nth-child(even')
-for(let i = 0; i<odd.length; i++){
-    odd[i].style.backgroundColor = 'green'
-    even[i].style.backgroundColor = '#f4f4f4'
-}
+// var odd = document.querySelectorAll('li:nth-child(odd')
+// var even = document.querySelectorAll('li:nth-child(even')
+// for(let i = 0; i<odd.length; i++){
+//     odd[i].style.backgroundColor = 'green'
+//     even[i].style.backgroundColor = '#f4f4f4'
+// }
+
+//Travesing the DOM
+//parentNode
+let itemList = document.querySelector('#items')
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+
+//parentElement
+// let itemList = document.querySelector('#items')
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4'
+
+//childNode
+// console.log(itemList.childNodes)
+
+//children
+// console.log(itemList.children)
+// itemList.children[1].style.backgroundColor = 'yellow'
+
+// firstChild
+// console.log(itemList.firstChild)
+// firstElementChild
+// console.log(itemList.firstElementChild);
+
+// lastChild
+// console.log(itemList.lastChild)
+// //lastElementChild
+// console.log(itemList.lastElementChild);
 
 
+//nextSibling
+// console.log(itemList.nextSibling)
+// //prevSibling
+// console.log(itemList.previousSibling)
 
+//nextElementSibling
+// console.log(itemList.nextElementSibling)
+//prevElementSibling
+
+// console.log(itemList.previousElementSibling)
+// itemList.previousElementSibling.style.color = 'green';
+
+//create an element
+//create a div
+let newDiv = document.createElement('div')
+//addClass
+newDiv.claasName = 'hello';
+//addId
+newDiv.id ='hello1'
+//addAtr
+newDiv.setAttribute('title', 'hello');
+
+//create text div
+let newDivText = document.createTextNode('Hello world')
+
+//add text to div
+newDiv.appendChild(newDivText);
+
+let container = document.querySelector('header .container')
+let h1 = document.querySelector('header h1')
+console.log(newDiv);
+newDiv.style.fontSize ='30px'
+container.insertBefore(newDiv,h1)
+
+let listItem = document.querySelector('#items')
+const newListItem = document.createElement('li');
+newListItem.innerHTML = 'Hello World';
+listItem.insertBefore(newListItem,itemList.firstChild)
 
 
 
